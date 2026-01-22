9c3302061a0925c7dd1c12e3af9c643281e78096
kingyost <31832631+kingyost@users.noreply.github.com>
2026-01-22 17:31:56 -0500

    base dice roller and multi dice roller

--- roller.py
+++ roller.py
@@ -1,15 +1,32 @@
 from random import randint as rand
 
-# def roll_dice(num):
-#     """
-#     return a list of integers with length `num`
-#     """
-#
-#     roll_result =[]
-#
-#     for side in range (num):
-#         roll = rand()
+def roll_dice(sides):
+    """
+    take in a number- 'sides', return a random number between 1 and 'sides'
+    """
+
+    try:
+        if sides > 0:
+            rolled = rand(1, sides)
+        else:
+            print("Did you just try to roll a die with 0 sides?")
+    except:
+        print("whoop, that wasn't a valid number. whole numbers only please.")
 
 
-def roll_norse():
-    roll_result = {}
+
+# TODO
+def multi_roll(die, sides):
+    """
+    roll multiple dice of the same size, add the results and return
+    """
+
+
+    for each die:
+        result = roll_dice(sides)
+        print(f"\nResult: [ {result} ]")
+        total += result
+
+
+# TODO
+# def roll_norse():
